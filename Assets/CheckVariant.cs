c36e3986e3d2d3710d57b8a2b7d4697fb2974516
Paul Sinnett <paul.sinnett@gmail.com>
2020-08-24 13:54:43 +0100

    Initial commit

---
+++ Assets/CheckVariant.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CheckVariant : MonoBehaviour
+{
+    public ShaderVariantCollection variants;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string [] keywords = new string [] {
+            "DIRECTIONAL",
+            "LIGHTPROBE_SH",
+            "_ALPHAPREMULTIPLY_ON"
+        };
+        ShaderVariantCollection.ShaderVariant variant =
+            new ShaderVariantCollection.ShaderVariant(
+                Shader.Find("Standard"),
+                PassType.ForwardBase,
+                keywords);
+        bool match = variants.Contains(variant);
+        Debug.LogFormat(
+            "Shader {0} pass {1} keywords {2} found {3}",
+            variant.shader.name,
+            variant.passType,
+            string.Join(" ", variant.keywords),
+            match);
+    }
+}
